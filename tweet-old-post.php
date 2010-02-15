0f151ead9aa55e963f2f425a1449ab6489d12405
ajaymatharu <ajaymatharu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-15 17:10:44 +0000

    git-svn-id: https://plugins.svn.wordpress.org/tweet-old-post/trunk@206058 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tweet-old-post.php
+++ tweet-old-post.php
@@ -4,7 +4,7 @@
 #     Plugin URI: http://www.ajaymatharu.com/wordpress-plugin-tweet-old-posts/
 #     Description: Plugin for tweeting your old posts randomly 
 #     Author: Ajay Matharu 
-#     Version: 1.4
+#     Version: 1.5
 #     Author URI: http://www.ajaymatharu.com
 #     */  
  
