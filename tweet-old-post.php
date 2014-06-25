3dd76eb329e2ea5766dcf3a6663aaff107346835
codeinwp <codeinwp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-25 20:12:25 +0000

    fixed various bugs
    git-svn-id: https://plugins.svn.wordpress.org/tweet-old-post/trunk@938716 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tweet-old-post.php
+++ tweet-old-post.php
@@ -4,24 +4,24 @@
 #     Plugin URI: http://themeisle.com/plugins/tweet-old-post-lite/
 #     Description: Wordpress plugin that helps you to keeps your old posts alive by sharing them and driving more traffic to them from twitter/facebook or linkedin. It also helps you to promote your content. You can set time and no of posts to share to drive more traffic.For questions, comments, or feature requests, <a href="http://themeisle.com/contact/?utm_source=plugindesc&utm_medium=announce&utm_campaign=top">contact </a> us!
 #     Author: ThemeIsle 
-#     Version: 6.7.9
+#     Version: 6.8
 #     Author URI: http://themeisle.com/
 #     */  
 
 // Config Constants
-define("PLUGINPATH", realpath(dirname(__FILE__) ));
-define("CSSFILE", plugins_url('css/style.css',__FILE__ ));
-define("CUSTOMDASHBOARDICON", plugins_url("css/custom_dashboard_icon.css", __FILE__));
-define("JSFILE", plugins_url('js/master.js',__FILE__ ));
-define("JSCOUNTDOWN", plugins_url('js/countdown.js',__FILE__ ));
-define("PLUGINBASENAME", plugin_basename(__FILE__));
-define('TOP_FB_API_VERSION','v2.0');
+define("ROPPLUGINPATH", realpath(dirname(__FILE__) ));
+define("ROPCSSFILE", plugins_url('css/style.css',__FILE__ ));
+define("ROPCUSTOMDASHBOARDICON", plugins_url("css/custom_dashboard_icon.css", __FILE__));
+define("ROPJSFILE", plugins_url('js/master.js',__FILE__ ));
+define("ROPJSCOUNTDOWN", plugins_url('js/countdown.js',__FILE__ ));
+define("ROPPLUGINBASENAME", plugin_basename(__FILE__));
+define('ROP_TOP_FB_API_VERSION','v2.0');
 // Require core.
-require_once(PLUGINPATH."/inc/core.php");
+require_once(ROPPLUGINPATH."/inc/core.php");
 // Require core.
-require_once(PLUGINPATH."/inc/exclude-posts.php");
+require_once(ROPPLUGINPATH."/inc/exclude-posts.php");
 if (!class_exists('TAV_Remote_Notification_Client')) {
-	require( PLUGINPATH.'/inc/class-remote-notification-client.php' );
+	require( ROPPLUGINPATH.'/inc/class-remote-notification-client.php' );
 }
 if (!function_exists('topProImage'))
     $notification = new TAV_Remote_Notification_Client( 37, 'a8be784b898fa2fb', 'http://themeisle.com?post_type=notification' );
